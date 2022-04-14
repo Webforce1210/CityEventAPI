bf4aaf620a65b41a799fbe4c1a4c8c510099c61a
Céline Foucart <celinefoucart@yahoo.fr>
2022-04-14 11:05:43 +0200

    Création EventController

---
+++ src/Controller/EventController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/event', name: 'app_event')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+
+    #[Route('/event/last', name: 'app_event_last')]
+    public function lastEvent(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+
+    #[Route('/event/{id}', name: 'app_event_show')]
+    public function show(int $id): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+
+    #[Route('/event/add', name: 'app_event_add')]
+    public function add(array $data): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+
+    #[Route('/event/{id}/update', name: 'app_event_update')]
+    public function update(array $data, int $id): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+
+    #[Route('/event/{id}/delete', name: 'app_event_delete')]
+    public function delete(int $id): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/EventController.php',
+        ]);
+    }
+}
